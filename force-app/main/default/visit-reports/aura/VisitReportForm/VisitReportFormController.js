61a9526179a0bd4ed12558c6ff2fb403dc9bd81c
Brian Kessler <kessler.bm@gmail.com>
2020-06-17 21:06:42 +0200

    Replaced logs and alert with toast. Removed more unused code.

--- force-app/main/default/visit-reports/aura/VisitReportForm/VisitReportFormController.js
+++ force-app/main/default/visit-reports/aura/VisitReportForm/VisitReportFormController.js
@@ -12,16 +12,32 @@
             userList: component.get('v.selectedUsers')
         });
         action.setCallback(this, function (response) {
+            let title = '';
+            let type = '';
+            let message = '';
             if ('SUCCESS' === response.getState()) {
-                alert('From server: ' + response.getReturnValue());
+                const value = response.getReturnValue();
+                title = 'Success';
+                type = 'success';
+                message = `'${value.Subject__c}' is Created Successfully as record ${value.Id}`;
             } else {
+                title = 'Error';
+                type = 'error';
+
                 const errors = response.getError();
                 const error = errors[0];
-                console.error((error && error.message)
+                message = (error && error.message)
                     ? `Error message: ${error}`
-                    : 'Unknown error'
-                );
+                    : 'Unknown error';
             }
+
+            const toastEvent = $A.get("e.force:showToast");
+            toastEvent.setParams({
+                "title": title,
+                "type": type,
+                "message": message
+            });
+            toastEvent.fire();
         });
 
         $A.enqueueAction(action);
